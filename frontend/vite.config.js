64a22b06275ef9c62da87c86fedae5e8f443fdf7
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2025-03-27 11:40:37 +0700

    vite.config.js update again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
@@ -21,7 +20,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist', // Pastikan hasil build masuk ke dist
-    emptyOutDir: true, // Bersihkan dist sebelum build baru
+    outDir: 'dist', // Tambahkan ini kalau belum ada
   },
+  base: './', // Penting untuk deployment ke Vercel
 })
