2631d07609670db64434e0d5dac0cba51848a31f
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2025-03-28 21:21:22 +0700

    Vite revert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,46 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Pisahkan library eksternal
-          if (id.includes('node_modules')) {
-            // Chunk spesifik untuk library utama
-            if (id.includes('vue') || id.includes('vue-router')) return 'vue-vendor'
-            if (id.includes('tailwindcss')) return 'tailwind-vendor'
-
-            // Chunk umum untuk library lain
-            return 'vendor'
-          }
-        },
-      },
-    },
-    // Batasan ukuran chunk (KB)
-    chunkSizeWarningLimit: 700,
-
-    // Kompresi tambahan
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
-  // Optimasi performa
-  optimizeDeps: {
-    include: ['vue', 'vue-router'],
-    exclude: ['@intlify/unplugin-vue-i18n'],
   },
 })
